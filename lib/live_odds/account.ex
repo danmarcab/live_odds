26b07217a6d0155c69434899308243553b9824c1
Daniel Marin <danmarcab@gmail.com>
2015-06-01 15:28:52 +0100

    2.1

--- lib/live_odds/account.ex
+++ lib/live_odds/account.ex
@@ -1,57 +1,29 @@
 defmodule LiveOdds.Account do
 
   def start do
-    pid = spawn(__MODULE__, :init, [])
-    Process.register(pid, :balance)
-    pid
+    Agent.start(fn() -> %{balance: 0} end, name: :account)
   end
 
-  def balance, do: call(:balance)
-  def credit(amount), do: call({:credit, amount})
-  def debit(amount), do: call({:debit, amount})
-
-
-  def init do
-    initialize_state
-    |> loop
+  def balance do
+    Agent.get(:account, fn(%{balance: balance}) -> balance end)
   end
 
-  defp call(message) do
-    send(:balance, {:request, self, message})
-    receive do
-      {:reply, reply} -> reply
+  def credit(amount) when is_integer(amount) and amount > 0 do
+    Agent.update(:account, fn(%{balance: balance} = state) ->
+      %{state| balance: balance + amount}
+    end)
+  end
+  def credit(invalid_amount), do: :error
+
+  def debit(amount) when is_integer(amount) and amount > 0 do
+    Agent.get_and_update(:account, &try_debit(&1, amount))
+  end
+  def debit(invalid_amount), do: :error
+
+  defp try_debit(%{balance: balance} = state, amount) do
+    cond do
+      balance < amount -> {:error, state}
+      true -> {:ok, %{state| balance: balance - amount}}
     end
   end
-
-  defp reply(pid, message) do
-    send(pid, {:reply, message})
-  end
-
-  defp loop(%{balance: balance} = state) do
-    receive do
-      {:request, from, :balance} ->
-        reply(from, balance)
-        loop(state)
-      {:request, from, {:debit, amount}} when amount > 0 and balance >= amount ->
-        reply(from, :ok)
-        loop(%{state| balance: balance - amount})
-      {:request, from, {:credit, amount}} when amount > 0 ->
-        reply(from, :ok)
-        loop(%{state| balance: balance + amount})
-      {:request, from, _} ->
-        reply(from, :error)
-        loop(state)
-      :stop -> terminate(state)
-    end
-
-  end
-
-  defp initialize_state do
-    %{balance: 0}
-  end
-
-  defp terminate(_state) do
-    :ok
-  end
-
 end
